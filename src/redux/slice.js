d85b34c494e9d7e2488c294d7d4b25cd5c59717c
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2023-04-23 20:13:57 +0200

    fixes1

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -3,8 +3,17 @@ import { nanoid } from 'nanoid';
 
 const LOCAL_STORAGE_CONTACTS = 'contacts';
 
+const contactsArray = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 const contactsInitialState = JSON.parse(
-  localStorage.getItem(LOCAL_STORAGE_CONTACTS)
+  localStorage.getItem(
+    LOCAL_STORAGE_CONTACTS ? LOCAL_STORAGE_CONTACTS : contactsArray
+  )
 );
 
 const contactsSlice = createSlice({
